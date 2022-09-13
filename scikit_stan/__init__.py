1ba732e5a32523d9b8f8ac5d90c70e09643f85b1
Brian Ward <bward@flatironinstitute.org>
2022-09-13 10:49:34 -0400

    Update loading order

--- scikit_stan/__init__.py
+++ scikit_stan/__init__.py
@@ -1,7 +1,8 @@
 from ._version import __version__  # noqa
-from .generalized_linear_regression import GLM
 from .utils.stan import init_local_cmdstan
 
 __all__ = ["GLM"]
 
 init_local_cmdstan()
+
+from .generalized_linear_regression import GLM
