fb8233f204bc0d959e37c186aa61c0c08b230865
Brian Ward <bward@flatironinstitute.org>
2022-10-12 12:30:07 -0400

    Use sklearn exception if available

--- scikit_stan/exceptions.py
+++ scikit_stan/exceptions.py
@@ -1,8 +1,15 @@
 """Custom exception types, as in sklearn."""
 
 
-class NotFittedError(ValueError, AttributeError):
-    """Exception class to raise if estimator is used before fitting.
-    This class inherits from both ValueError and AttributeError to help with
-    exception handling and backward compatibility.
-    """
+try:
+    from sklearn.exceptions import NotFittedError
+except ImportError:
+
+    class NotFittedError(ValueError, AttributeError):  # type:ignore
+        """Exception class to raise if estimator is used before fitting.
+        This class inherits from both ValueError and AttributeError to help with
+        exception handling and backward compatibility.
+        """
+
+
+__all__ = ["NotFittedError"]
